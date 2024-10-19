812dbbedf4b7ea22d0af20f443944ebdfe0849be
Justin <59674603+Toash@users.noreply.github.com>
2024-10-19 16:56:49 -0400

    Add loggedIn status for navbar for appropriate rendering

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -20,6 +20,7 @@ import { useNavigate } from "react-router-dom";
 import FlexBetween from "components/FlexBetween";
 import logout from "auth/logout";
 import useUserById from "data/useUserById";
+import checkAuthenticatedUser from "data/checkAuthenticatedUser";
 
 const NavBar = () => {
   const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
@@ -48,137 +49,137 @@ const NavBar = () => {
     return <Typography>Loading...</Typography>
   }
 
-  if (isSuccess) {
 
-    const fullName = data.firstName + data.lastName;
+  const loggedIn = checkAuthenticatedUser();
+  const fullName = data.firstName + data.lastName;
 
-    return (
-      <FlexBetween padding="1rem 6%" backgroundColor={alt}>
-        <FlexBetween gap="1.75rem">
-          <Typography
-            fontWeight="bold"
-            fontSize="clamp(1rem,2rem,2.25rem)"
-            color="primary"
-            onClick={() => navigate("/")}
-            sx={{
-              "&:hover": {
-                color: primaryLight,
-                cursor: "pointer",
-              },
-            }}
-          >
-            Climbing App
-          </Typography>
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem,2rem,2.25rem)"
+          color="primary"
+          onClick={() => navigate("/")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          BoulderStat
+        </Typography>
+      </FlexBetween>
+      {/* DESKTOP NAV */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <FormControl variant="standard" value={fullName}>
+            {loggedIn ? (
+              <Select
+                value={fullName}
+                sx={{
+                  backGroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase></InputBase>}
+              >
+                <MenuItem value={fullName}>
+                  <Typography>{fullName}</Typography>
+                </MenuItem>
+                <MenuItem onClick={() => logout()}>
+                  Log Out
+                </MenuItem>
+              </Select>
+            ) : (
+              signInButton
+            )}
+          </FormControl>
         </FlexBetween>
-        {/* DESKTOP NAV */}
-        {isNonMobileScreens ? (
-          <FlexBetween gap="2rem">
+      ) : (
+        <IconButton
+          onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+        >
+          <Menu></Menu>
+        </IconButton>
+      )}
+
+      {/* MOBILE NAV */}
+      {!isNonMobileScreens && isMobileMenuToggled && (
+        <Box
+          position="fixed"
+          right="0"
+          bottom="0"
+          height="100%"
+          zIndex="10"
+          maxWidth="500px"
+          minWidth="300px"
+          backgroundColor={background}
+        >
+          {/* CLOSE ICON */}
+          <Box display="flex" justifyContent="flex-end" p="1rem">
+            <IconButton
+              onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+            >
+              <Close> </Close>
+            </IconButton>
+          </Box>
+
+          {/* MENU ITEMS */}
+          <FlexBetween
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap="3rem"
+          >
             <FormControl variant="standard" value={fullName}>
-              {isSuccess ? (
-                <Select
-                  value={fullName}
-                  sx={{
-                    backGroundColor: neutralLight,
-                    width: "150px",
-                    borderRadius: "0.25rem",
-                    p: "0.25rem 1rem",
-                    "& .MuiSvgIcon-root": {
-                      pr: "0.25rem",
-                      width: "3rem",
-                    },
-                    "& .MuiSelect-select:focus": {
-                      backgroundColor: neutralLight,
-                    },
-                  }}
-                  input={<InputBase></InputBase>}
-                >
-                  <MenuItem value={fullName}>
-                    <Typography>{fullName}</Typography>
-                  </MenuItem>
-                  <MenuItem onClick={() => logout()}>
-                    Log Out
-                  </MenuItem>
-                </Select>
-              ) : (
-                signInButton
-              )}
+              <Select
+                value={fullName}
+                sx={{
+                  backGroundColor: neutralLight,
+                  width: "150px",
+                  borderRadius: "0.25rem",
+                  p: "0.25rem 1rem",
+                  "& .MuiSvgIcon-root": {
+                    pr: "0.25rem",
+                    width: "3rem",
+                  },
+                  "& .MuiSelect-select:focus": {
+                    backgroundColor: neutralLight,
+                  },
+                }}
+                input={<InputBase></InputBase>}
+              >
+                {loggedIn ? (
+                  <>
+                    <MenuItem value={fullName}>
+                      <Typography>{fullName}</Typography>
+                    </MenuItem>
+                    <MenuItem onClick={() => logout()}>
+                      Log Out
+                    </MenuItem>
+                  </>
+                ) : (
+                  signInButton
+                )}
+              </Select>
             </FormControl>
           </FlexBetween>
-        ) : (
-          <IconButton
-            onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-          >
-            <Menu></Menu>
-          </IconButton>
-        )}
+        </Box>
+      )}
+    </FlexBetween>
+  );
+};
 
-        {/* MOBILE NAV */}
-        {!isNonMobileScreens && isMobileMenuToggled && (
-          <Box
-            position="fixed"
-            right="0"
-            bottom="0"
-            height="100%"
-            zIndex="10"
-            maxWidth="500px"
-            minWidth="300px"
-            backgroundColor={background}
-          >
-            {/* CLOSE ICON */}
-            <Box display="flex" justifyContent="flex-end" p="1rem">
-              <IconButton
-                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
-              >
-                <Close> </Close>
-              </IconButton>
-            </Box>
-
-            {/* MENU ITEMS */}
-            <FlexBetween
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-              gap="3rem"
-            >
-              <FormControl variant="standard" value={fullName}>
-                <Select
-                  value={fullName}
-                  sx={{
-                    backGroundColor: neutralLight,
-                    width: "150px",
-                    borderRadius: "0.25rem",
-                    p: "0.25rem 1rem",
-                    "& .MuiSvgIcon-root": {
-                      pr: "0.25rem",
-                      width: "3rem",
-                    },
-                    "& .MuiSelect-select:focus": {
-                      backgroundColor: neutralLight,
-                    },
-                  }}
-                  input={<InputBase></InputBase>}
-                >
-                  {loggedIn ? (
-                    <>
-                      <MenuItem value={fullName}>
-                        <Typography>{fullName}</Typography>
-                      </MenuItem>
-                      <MenuItem onClick={() => logout()}>
-                        Log Out
-                      </MenuItem>
-                    </>
-                  ) : (
-                    signInButton
-                  )}
-                </Select>
-              </FormControl>
-            </FlexBetween>
-          </Box>
-        )}
-      </FlexBetween>
-    );
-  };
-}
 
 export default NavBar;
