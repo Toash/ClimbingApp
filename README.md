1447fa5d4cd0d5ed43a3d55a457f33cae5982970
Justin <iitoash@gmail.com>
2025-02-20 16:11:01 -0500

    only display week when user is logged in. readme update

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-
 Live Link: https://boulderstat.com
 
 # Things that I learned
+
 - For token authentication and refreshing, need to handle cases when access / id token becomes invalid, can wrap fetch function with a function that checks for unauthorized response, if so refresh the token. If using axios you can use intercepters instead to check for the unauthorized response and then refresh.
 
 - Data fetching from an api requires multiple features to be efficient such as caching, making data stale (Keeping data up to date), response handling for success, loading, and error. React query makes this much easier, it can keep track of each data fetched from an api through a key, and this can be fetched or invalidated anywhere in the application.
@@ -17,22 +17,27 @@ Live Link: https://boulderstat.com
 # About the project
 
 ## Frontend
+
 S3 bucket, Cloudfront. (Uses MUI and inline styling) <br  />
 
 ## Backend
+
 API Gateway into lambda with serverless-express, S3 bucket to store media, MongoDB to store user data <br  />
 
 Using React Query, the api data is fetch and stored along with a key, when fetching the same key, we get the cached result instead of sending the same request. Data will expire (become stale), after some time, or can become invalidated (mutations will invalidate their respective data.)
 
 ## Authentication
+
 Cognito to manage Authentication, using OAuth2 flow. Getting authentiation code from hosted ui, sending request to token endpoint to get the tokens. Storing refresh token in a secure cookie to get new tokens if they expire. <br  />
 
 ## Media upload / storage
+
 Hit backend endpoint to get presigned url, use this to upload. Use object url to get media as the bucket is public. <br/>
 
 S3 bucket uses the Cognito user id as a folder name for each user to store images / videos. <br/>
 
 ## Staging environment.
+
 Uses seperate cloudfront distribution and s3 bucket
 
 Seperate API Gateway stages
@@ -41,18 +46,11 @@ Seperate lambda
 
 Same user pool with different app client
 
-  
-  
-  
+## Notes
+
+npm run dev to run frontend, connected to staging backend (loads in the env file assuming you have it)
+push.sh to push to production / staging to update frontend and or backend on AWS.
 
 ## WIP
 
-  
-
 https://trello.com/b/1NERCnDG/climbing-app <br/>
-
-  
-  
-
-
-
--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { Box, CircularProgress, Typography, useMediaQuery, IconButton, Modal, LinearProgress } from "@mui/material";
+import {
+  Box,
+  CircularProgress,
+  Typography,
+  useMediaQuery,
+  IconButton,
+  Modal,
+  LinearProgress,
+} from "@mui/material";
 import NavBar from "scenes/navbar";
 import CurrentUserCard from "scenes/widgets/CurrentUserCard";
 import LogClimbForm from "scenes/widgets/LogClimbForm";
@@ -8,7 +16,12 @@ import Posts from "scenes/widgets/Posts";
 import FriendListWidget from "scenes/widgets/FriendListWidget";
 import CurrentUserStats from "scenes/widgets/CurrentUserStats";
 import { QUERY_KEYS } from "queryKeys";
-import { useIsMutating, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  useIsMutating,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import Week from "scenes/widgets/Week.jsx";
 import SideDrawer from "scenes/drawer/SideDrawer.jsx";
 import EditAccount from "scenes/widgets/EditAccount.jsx";
@@ -19,52 +32,50 @@ import { enqueueSnackbar } from "notistack";
 import CollapsibleWidgetWrapper from "components/CollapsibleWidgetWrapper.jsx";
 
 const HomePage = () => {
-
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   const queryClient = useQueryClient();
 
   /**
    * Exchanges the auth code for tokens, sets the tokens in local storage.
    */
-  const exchangeCodeMutation = useMutation(
-    {
-      mutationKey: ["exchangeCode"],
-      mutationFn: async (authorizationCode) => {
-        const response = await fetch(
-          import.meta.env.VITE_APP_API_BASE_URL + `/auth/exchange-code`,
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ authorizationCode }),
-          }
-        );
-        return await response.json();
-      },
-      onSuccess: (data) => {
-        const { id_token, access_token } = data;
-        console.log(data)
-
-        // invalid grant will make access_token and id_token undefined.
-        if (access_token && id_token) {
-          localStorage.setItem("access_token", access_token);
-          localStorage.setItem("id_token", id_token);
+  const exchangeCodeMutation = useMutation({
+    mutationKey: ["exchangeCode"],
+    mutationFn: async (authorizationCode) => {
+      const response = await fetch(
+        import.meta.env.VITE_APP_API_BASE_URL + `/auth/exchange-code`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ authorizationCode }),
         }
+      );
+      return await response.json();
+    },
+    onSuccess: (data) => {
+      const { id_token, access_token } = data;
+      console.log(data);
 
-        // Refetch user profile with the new token
-        queryClient.invalidateQueries(QUERY_KEYS.CURRENT_USER);
-
-        // TODO display success message
-        console.log("Successfully got new tokens.!", { access_token, id_token })
-      },
-      onError: (error) => {
-        console.error('Error exchanging auth code', error);
-        localStorage.removeItem("access_token")
-        localStorage.removeItem("id_token")
+      // invalid grant will make access_token and id_token undefined.
+      if (access_token && id_token) {
+        localStorage.setItem("access_token", access_token);
+        localStorage.setItem("id_token", id_token);
       }
-    });
+
+      // Refetch user profile with the new token
+      queryClient.invalidateQueries(QUERY_KEYS.CURRENT_USER);
+
+      // TODO display success message
+      console.log("Successfully got new tokens.!", { access_token, id_token });
+    },
+    onError: (error) => {
+      console.error("Error exchanging auth code", error);
+      localStorage.removeItem("access_token");
+      localStorage.removeItem("id_token");
+    },
+  });
 
   /**
-   * 
+   *
    * @returns {boolean} true if user has valid token, false otherwise
    */
   const userHasValidToken = async () => {
@@ -90,7 +101,6 @@ const HomePage = () => {
     }
   };
 
-
   /**
    * If there is a code in url and user does not already have a token, attempt to get them.
    */
@@ -100,7 +110,7 @@ const HomePage = () => {
 
     // console.log("Authorization code extracted: ", authorizationCode)
 
-    if (authorizationCode && !await userHasValidToken()) {
+    if (authorizationCode && !(await userHasValidToken())) {
       exchangeCodeMutation.mutate(authorizationCode);
     }
   };
@@ -110,41 +120,61 @@ const HomePage = () => {
     getTokensFromAuthCode();
   }, []);
 
-
-  const { isSuccess, isLoading, isError, error, data } = useAuthenticatedUser()
+  const { isSuccess, isLoading, isError, error, data } = useAuthenticatedUser();
 
   // after we have gotten the user profile
-  const [needToSetupUserProfile, _] = useState(isSuccess && !(!data?.firstName || !data?.lastName))
+  const [needToSetupUserProfile, _] = useState(
+    isSuccess && !(!data?.firstName || !data?.lastName)
+  );
 
   // make sure state occurs before early return, or else hooks will be out of order.
-  const [editAccountOpen, setEditAccountOpen] = useState(needToSetupUserProfile);
+  const [editAccountOpen, setEditAccountOpen] = useState(
+    needToSetupUserProfile
+  );
 
   if (isLoading) {
     return (
       <>
-        <Box sx={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Box>
             <Typography>Loading user profile...</Typography>
             <CircularProgress />
           </Box>
         </Box>
       </>
-    )
+    );
   }
   if (isError) {
     return (
       <>
-        <Box sx={{ width: "100%", height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <Box
+          sx={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Box>
-            <Typography>Error tryinng to fetch user profile. Please try again later</Typography>
+            <Typography>
+              Error tryinng to fetch user profile. Please try again later
+            </Typography>
           </Box>
         </Box>
         {/* {queryClient.removeQueries({ queryKey: QUERY_KEYS.CURRENT_USER })} */}
         {console.log("Error trying to fetch user profile", error)}
       </>
-    )
+    );
   }
-
 
   if (needToSetupUserProfile) {
     //console.log("User profile not set up.")
@@ -152,29 +182,27 @@ const HomePage = () => {
 
   const handleEditAccountOpen = () => {
     setEditAccountOpen(true);
-  }
+  };
 
   const handleEditAccountClose = (event, reason) => {
     if (needToSetupUserProfile) {
       if (reason && reason === "backdropClick") {
-        enqueueSnackbar("Please complete profile setup to continue.")
+        enqueueSnackbar("Please complete profile setup to continue.");
         return;
       }
     }
     setEditAccountOpen(false);
-  }
-
-
-
-
-
-
+  };
 
   if (isNonMobileScreens) {
     // Desktop layout
     return (
       <>
-        <EditAccount open={editAccountOpen} onClose={handleEditAccountClose} firstTime={needToSetupUserProfile} />
+        <EditAccount
+          open={editAccountOpen}
+          onClose={handleEditAccountClose}
+          firstTime={needToSetupUserProfile}
+        />
         <Box>
           <Box
             width="100%"
@@ -183,8 +211,7 @@ const HomePage = () => {
             gap="2rem"
             justifyContent="space-between"
           >
-            <SideDrawer
-            />
+            <SideDrawer />
             <Box
               flexGrow={3}
               display="flex"
@@ -192,10 +219,11 @@ const HomePage = () => {
               alignItems={"center"}
               gap="2rem"
             >
-
-              <CollapsibleWidgetWrapper label="Week" defaultExpanded>
-                <Week style={{ width: "100%" }} />
-              </CollapsibleWidgetWrapper>
+              {isSuccess && (
+                <CollapsibleWidgetWrapper label="Week" defaultExpanded>
+                  <Week style={{ width: "100%" }} />
+                </CollapsibleWidgetWrapper>
+              )}
 
               <Posts />
             </Box>
@@ -206,30 +234,44 @@ const HomePage = () => {
             >
               {isSuccess && (
                 <>
-                  <Box sx={{ display: "flex", flexDirection: "column", position: "sticky", top: "1rem", gap: "2rem", minWidth: "300px" }}>
-                    <CurrentUserCard handleEditAccount={handleEditAccountOpen} />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      position: "sticky",
+                      top: "1rem",
+                      gap: "2rem",
+                      minWidth: "300px",
+                    }}
+                  >
+                    <CurrentUserCard
+                      handleEditAccount={handleEditAccountOpen}
+                    />
                     <CurrentUserStats />
                   </Box>
                 </>
               )}
             </Box>
           </Box>
-        </Box >
+        </Box>
       </>
     );
   } else {
     // Mobile layout
     return (
       <>
-        <EditAccount open={editAccountOpen} onClose={handleEditAccountClose} firstTime={needToSetupUserProfile} />
+        <EditAccount
+          open={editAccountOpen}
+          onClose={handleEditAccountClose}
+          firstTime={needToSetupUserProfile}
+        />
         <Box m="0 1rem">
           <Box
             width="100%"
             // widgets will be on top of eachother on mobile
             display={"block"}
           >
-            <SideDrawer
-            />
+            <SideDrawer />
             <Box
               //flexBasis={isNonMobileScreens ? "70%" : undefined}
               // margin for mobile since widgets are stacked
@@ -243,7 +285,6 @@ const HomePage = () => {
                 <>
                   <CurrentUserCard handleEditAccount={handleEditAccountOpen} />
                   <CollapsibleWidgetWrapper label="Stats">
-
                     <CurrentUserStats showTitle={false} />
                   </CollapsibleWidgetWrapper>
 
@@ -253,15 +294,13 @@ const HomePage = () => {
                 </>
               )}
 
-
               <Posts />
             </Box>
           </Box>
-        </Box >
+        </Box>
       </>
     );
   }
-}
-
+};
 
 export default HomePage;
